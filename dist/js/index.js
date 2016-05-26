de971fff4c11496cc91d8550d775b20ceca698e1
zhangtianxiang <zhangtianxiang@ucfgroup.com>
2016-05-26 10:08:22 +0800

    快速提交

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,3 +1,21 @@
-$(function(){
-    $('#iframeTag').height(screen.height);
-});
+class Animal{
+    constructor(kind){
+        this.kind=kind;
+    }
+    singIng(){
+        console.log(this.kind+' is singing');
+    }
+}
+class Cat extends Animal{
+    constructor(name){
+       super('dog');
+        this.name=name;
+    }
+    static distance(){
+        console.log('cat is distance');
+    }
+}
+
+var cat=new Cat();
+cat.singIng();
+Cat.distance();
