03cdd3127d2326856c3551cef712783ff90854b2
zhangtianxiang <zhangtianxiang@ucfgroup.com>
2016-05-26 14:31:38 +0800

    console.log('hehe');

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,21 +1,3 @@
-class Animal{
-    constructor(kind){
-        this.kind=kind;
-    }
-    singIng(){
-        console.log(this.kind+' is singing');
-    }
+function testFn(){
+    console.log('hehe');
 }
-class Cat extends Animal{
-    constructor(name){
-       super('dog');
-        this.name=name;
-    }
-    static distance(){
-        console.log('cat is distance');
-    }
-}
-
-var cat=new Cat();
-cat.singIng();
-Cat.distance();
