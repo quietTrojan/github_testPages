e73a7357dd15217e34d304018ecf9689352653f6
zhangtianxiang <zhangtianxiang@ucfgroup.com>
2016-06-02 15:33:46 +0800

    字体测试

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,3 +1,7 @@
 $(function(){
-    console.log('呵呵呵呵');
+    var mainBox=$('#mainBox');
+    var arr=["music", "search", "mail", "heart", "star", "user", "videocam", "camera", "photo", "attach", "lock", "eye", "tag", "pencil", "comment", "location", "cup", "trash", "doc", "note", "cog", "params", "calendar", "sound", "clock", "lightbulb", "tv", "desktop", "mobile", "cd", "inbox", "globe", "cloud", "fire", "megaphone", "database", "key", "beaker", "truck", "money", "food", "shop", "diamond", "wallet"];
+    for(var i=0;i<arr.length;i++){
+        $('<div>').addClass('linecons-'+arr[i]).appendTo(mainBox);
+    }
 })
