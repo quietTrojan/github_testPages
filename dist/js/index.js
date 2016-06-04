06c7658c0d644179cfcbbb167a06ec3f71ba7e75
zhangtianxiang <zhangtianxiang@ucfgroup.com>
2016-06-04 08:46:51 +0800

    快速提交

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,7 +1,5 @@
 $(function(){
-    var mainBox=$('#mainBox');
-    var arr=["music", "search", "mail", "heart", "star", "user", "videocam", "camera", "photo", "attach", "lock", "eye", "tag", "pencil", "comment", "location", "cup", "trash", "doc", "note", "cog", "params", "calendar", "sound", "clock", "lightbulb", "tv", "desktop", "mobile", "cd", "inbox", "globe", "cloud", "fire", "megaphone", "database", "key", "beaker", "truck", "money", "food", "shop", "diamond", "wallet"];
-    for(var i=0;i<arr.length;i++){
-        $('<div>').addClass('linecons-'+arr[i]).appendTo(mainBox);
-    }
+    $('#mainBox div').addClass(function(){
+        return 'myClass'+$(this).index()+' newClass';
+    });
 })
