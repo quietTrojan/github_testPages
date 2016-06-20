4efe381a15c00c1bd52c94949bc1656bd2d01a94
zhangtianxiang <zhangtianxiang@ucfgroup.com>
2016-06-20 10:42:51 +0800

    快速提交

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,11 +1,5 @@
 $(function(){
-    var headerScroll=new IScroll('#iscroll_header',{
-        scrollX:true,
-        scrollY:false,
-        bounce:false,
-        tap:true
-    });
-    $('#iscrollUl').on('tap','li',function(){
-        alert($(this).text());
+    $('#inputTag').on('click',function(event){
+        event.preventDefault();
     });
 });
